da1b6d0a791d3a5c60a7639f4035d35010b4a8dc
jfsilva12345 <jfsilva@unicauca.edu.co>
2021-12-21 17:08:17 -0500

    push

--- src/cliente/ClienteDeObjetos.java
+++ src/cliente/ClienteDeObjetos.java
@@ -16,4 +16,102 @@ public class ClienteDeObjetos{
         objRemoto = (GesUsuarioInt) UtilidadesRegistroC.obtenerObjRemoto(direccionIpRMIRegistry,numPuertoRMIRegistry,"ObjetoRemotoPersonal");
         MenuPrincipal();
     }
+    
+
+    private static void MenuPrincipal()
+    {
+        int opcion = 0;
+        do
+        {
+            System.out.println("==Menu==");
+            System.out.println("1. Registrar Usuario");			
+            System.out.println("2. Consultar Cantidad de Usuarios");
+            System.out.println("3. Consultar usuario");
+            System.out.println("4. Salir");
+
+            opcion = UtilidadesConsola.leerEntero();
+
+            switch(opcion)
+            {
+                case 1:
+                        Opcion1();
+                        break;
+                case 2:
+                        Opcion2();
+                        break;	
+                case 3:
+                        Opcion3();
+                        break;
+                case 4:
+                        System.out.println("Salir...");
+                        break;
+                default:
+                        System.out.println("Opción incorrecta");
+            }
+
+        }while(opcion != 4);
+    }
+
+    private static void Opcion1() 
+    {
+        try
+        {
+            System.out.println("==Registro del Cliente==");
+            System.out.println("Ingrese la identificacion");
+            int id = UtilidadesConsola.leerEntero();
+            System.out.println("Ingrese el nombre completo ");
+            String nombres = UtilidadesConsola.leerCadena();
+            System.out.println("Ingrese los apellidos ");
+            String apellidos = UtilidadesConsola.leerCadena();
+            UsuarioDTO objUsuario= new UsuarioDTO(id, nombres, apellidos);
+
+            boolean valor = objRemoto.registrarUsuario(objUsuario);//invocación al método remoto
+            if(valor)
+                    System.out.println("Registro realizado satisfactoriamente...");
+            else
+                    System.out.println("no se pudo realizar el registro...");
+        }
+        catch(RemoteException e)
+        {
+            System.out.println("La operacion no se pudo completar, intente nuevamente...");
+        }
+    }
+
+    private static void Opcion2()
+    {	
+        try
+        {
+            System.out.println("==Numero de usuarios==");
+            int tamanio  = objRemoto.consultarCantidadUsuarios();
+            System.out.println("El numero de usuarios registrados es de: "+ tamanio);
+        }
+        catch(RemoteException e)
+        {
+            System.out.println("La operación no se pudo completar, intente nuevamente...");
+            System.out.println("Excepcion generada: " + e.getMessage());
+        }	
+    }
+
+    private static void Opcion3() 
+    {
+        try
+        {
+            System.out.println("==Consulta de un Cliente==");
+            System.out.println("Ingrese la identificacion");
+            int id = UtilidadesConsola.leerEntero();			
+
+            UsuarioDTO objUsuario= objRemoto.consultarUsuario(id);
+            if(objUsuario!=null)
+            {
+                    System.out.println("Nombres: " + objUsuario.getNombres());
+                    System.out.println("Apellidos: " + objUsuario.getApellidos());
+            }
+            else
+                    System.out.println("Usuario no encontrado");
+        }
+        catch(RemoteException e)
+        {
+            System.out.println("La operacion no se pudo completar, intente nuevamente...");
+        }
+    }
 }
