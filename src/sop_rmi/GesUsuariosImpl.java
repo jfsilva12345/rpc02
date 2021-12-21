943f126859377956fff312bd6fe39d29dcb53b36
jfsilva12345 <jfsilva@unicauca.edu.co>
2021-12-21 17:13:51 -0500

    push

--- src/sop_rmi/GesUsuariosImpl.java
+++ src/sop_rmi/GesUsuariosImpl.java
@@ -9,16 +9,37 @@ public class GesUsuariosImpl extends UnicasRemoteObject implements GesUsuariosIn
     private int contador=0;
     
     public class GesUsuariosImpl() throws RemoteException{
-
+        super();
+        this.usuarios = new ArrayList();
     }
 
     @override
     public boolean registrarUsuario(PersonalDTO objUsuario) throws RemoteException{
-
+        System.out.println("Entrando a registrar usuario");
+        boolean bandera=false;
+        
+        if(misUsuarios.size() < 5)
+        {            
+            bandera=misUsuarios.add(objUsuario);
+            System.out.println("Usuario registrado: identificación: " + objUsuario.getID() + ", nombres: " + objUsuario.getNombres() + ", apellidos. " +objUsuario.getApellidos() );
+        }
+        
+        return bandera;
     }
 
     @override
     public PersonalDTO consultarUsuario(int id) throws RemoteException{
         
+        System.out.println("Entrando a consultar usuario");
+        UsuarioDTO objUsuario=null;
+        
+        for (int i = 0; i < this.misUsuarios.size(); i++) {
+            if(this.misUsuarios.get(i).getID()==identificacion)
+            {
+                objUsuario=this.misUsuarios.get(i);
+                break;
+            }
+        }
+        return objUsuario;  
     }
 }
